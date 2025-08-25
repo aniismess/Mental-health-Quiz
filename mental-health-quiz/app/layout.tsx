96efcd2a5cc48d2ca6f732c4ca2ed6f6d6dbd35b
Animesh mishra <142673038+aniismess@users.noreply.github.com>
2025-08-25 19:13:03 +0000

    Update layout.tsx

--- mental-health-quiz/app/layout.tsx
+++ mental-health-quiz/app/layout.tsx
@@ -5,9 +5,9 @@ import Link from 'next/link'; // Import Link component
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Mental Health',
+  description: '',
+  generator: '',
 }
 
 export default function RootLayout({
